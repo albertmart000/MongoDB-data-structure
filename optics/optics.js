6b14387e513f8b06f77a253f06f324fd935df75e
albertmart000 <98390165+albertmart000@users.noreply.github.com>
2022-09-09 10:34:11 +0200

    Update optics.js

--- optics/optics.js
+++ optics/optics.js
@@ -1,4 +1,4 @@
-var MongoClient = require('/home/albert/Escritorio/MongoDBProjects/node_modules/mongodb').MongoClient;
+var MongoClient = require('/node_modules/mongodb').MongoClient;
 const assert = require('assert');
 const url = 'mongodb://localhost:27017';
 use ('optics');
